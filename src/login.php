1ec0daaa37fbc4f89312f0872a31de6f78e3c6f0
VanessaHo99 <vanessahojingmei@gmail.com>
2022-11-17 14:45:49 +0800

    login

---
+++ src/login.php
@@ -0,0 +1,9 @@
+<?php 
+    <form method="post" action="" name="signin-form">
+    <div class="form-element">
+        <label>Password</label>
+        <input type="password" name="password" required />
+    </div>
+    <button type="submit" name="login" value="login">Log In</button>
+        </form>
+?>
